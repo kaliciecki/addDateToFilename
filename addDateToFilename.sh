a51ae2149d0f7bcb677792aec6720c235949358f
kaliciecki <kaliceckid@gmail.com>
2018-03-25 00:14:00 +0000

    Main file added.

--- addDateToFilename.sh
+++ addDateToFilename.sh
@@ -2,7 +2,6 @@
 
 #Create a script which will take a filename as its first argument and create a dated copy of the file. eg. if our file was named file1.txt it would create a copy such as 2018-03-24_file1.txt.
 
-
 file=$1
 
 addDateToFilename(){
